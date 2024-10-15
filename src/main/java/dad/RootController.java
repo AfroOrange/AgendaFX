68296bad98b6cf6d99323773c77f903ebdf92619
AfroOrange <nuhacet1998@gmail.com>
2024-10-15 12:24:30 +0100

    App de un bloc de notas con javaFX

---
+++ src/main/java/dad/RootController.java
@@ -0,0 +1,13 @@
+package dad;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RootController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
